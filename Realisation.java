b046c985464ace1eef91d991d0bc6baf42c10dc4
VecteurOM <164197085+VecteurOM@users.noreply.github.com>
2024-03-22 11:17:46 +0100

    Realisation.java

---
+++ Realisation.java
@@ -0,0 +1,19 @@
+package projetFilms;
+
+public abstract class Realisation extends Films {
+  private String titre;
+  private Date dateProd;
+  private String resume;
+  private double prix;
+  private double moyenne;
+  private Genre genre;
+
+  public Realisation(String titre, Date dateProd, String resume, double prix, double moyenne, Genre genre) {
+    this.titre = titre;
+    this.dateProd = dateProd;
+    this.resume = resume;
+    this.prix = prix;
+    this.moyenne = moyenne;
+    this.genre = genre;
+  }
+}
