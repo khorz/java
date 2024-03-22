4d9c42abe58441b689fa66205346e030fdc811d6
VecteurOM <164197085+VecteurOM@users.noreply.github.com>
2024-03-22 11:05:32 +0100

    Acteur.java

---
+++ Acteur.java
@@ -0,0 +1,11 @@
+package projetFilms;
+
+public class Acteur extends Films {
+  private String nom;
+  private String prenom;
+
+  public Acteur(String nom, String prenom) {
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+}
