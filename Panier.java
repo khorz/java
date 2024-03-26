3e3246a5e6e97fe0f9fad1559c9ea3b51d010760
xeu974 <164197253+xeu974@users.noreply.github.com>
2024-03-26 13:42:15 +0100

    Update Panier.java

--- Panier.java
+++ Panier.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.ArrayList;
+import java.util.Random;
+
 
 public class Panier {
 	private List<Film> selection;
@@ -101,13 +103,156 @@ public class Panier {
 		return catalogueAffiche;
 	}
 	
+	
 	public void rafraichirCatalogue() {
+		List<Film> tmp = new ArrayList<Film>();
+		Film recupJson[] = new Film[100];//constituer la liste depuis le json
+		int n = recupJson.length;
+		Random rand = new Random();
+		for (int i=0;i<catalogue.size();i++) {
+            int randomIndex = rand.nextInt(n);
+            if (!catalogue.contains(recupJson[randomIndex])) {
+            	tmp.add(recupJson[randomIndex]);
+            }package projetFilms;
+import java.util.List;
+
+
+public class Panier {
+	private List<Film> selection;
+	//private List<Film> catalogue;
+	
+	public Panier(List<Film> selection) {
+		this.selection = selection;
+		//this.catalogue = catalogue;
+	}
+	
+	 
+	public List<Film> getSelection() {
+		return selection;
+	}
+
+
+	public void setSelection(List<Film> selection) {
+		this.selection = selection;
+	}
+
+
+	
+
+
+	
+	// Methodes UML
+	
+	public void ajouterFilm(List<String> selection, String codeFilm) {
+		
+		if (selection.contains(codeFilm)) {
+			System.out.println("Le film est déjà séléctionné");
+		}
+		else {
+			selection.add(codeFilm);
+		}
+	}
+	
+	public void supprimerFilm(List<String> selection, String codeFilm) {
+		if (selection.contains(codeFilm)) {
+			selection.remove(codeFilm);
+		}
+		else {
+			System.out.println("Le film n'est pas dans la sélection");
+		}
+	}
+	
+	public static void trierFilmsParDate(List<Film> listeFilms) {
+		
+        // Utiliser la méthode sort de la classe Collections avec un Comparator personnalisé
+        Collections.sort(listeFilms, new Comparator<Film>() {
+            public int compare(Film film1, Film film2) {
+                return film1.getDateProd().compareTo(film2.getDateProd());
+            }
+        });
+	}
+	
+	List<Film> catalogue;
+	
+	
+	//int tailleCatalogue = 10;
+	
+	public void ajouterAuCatalogue(Film film) {
+		if (catalogue.contains(film)){
+			System.out.println("Deja ajouté");
+		}
+		else {
+			catalogue.add(film);
+			System.out.println("Nouveau film disponible");
+		}
+	}
+	
+	int tailleCatalogueAffiche = 10;
+	
+	public List<Film> afficherCatalogue(Utilisateur user){
+		
+		int pourcentageFilmRecent = tailleCatalogueAffiche*70/100;
+		int pourcentageAutre = tailleCatalogueAffiche - pourcentageFilmRecent;
+		
+		List<Film> catalogueAffiche = new ArrayList<Film>();
+		Genre genrePrefere = user.genrePrefere();
+		for (Film elt : catalogue) {
+			if (!catalogue.contains(elt) && catalogueAffiche.size()<=pourcentageFilmRecent && genrePrefere==elt.getGenre()) {
+				catalogueAffiche.add(elt);
+			}
+		}
+		
+		trierFilmsParDate(catalogue);
+		for (Film elt : catalogue) {
+			if (!catalogue.contains(elt) && catalogueAffiche.size()<= pourcentageAutre) {
+				catalogueAffiche.add(elt);
+			}
+		}
+		
+		
+		return catalogueAffiche;
+	}
+	
+	
+	public void rafraichirCatalogue() {
+		List<Film> tmp = new ArrayList<Film>();
+		Film recupJson[] = new Film[100];//constituer la liste depuis le json
+		int n = recupJson.length;
+		Random rand = new Random();
+		for (int i=0;i<catalogue.size();i++) {
+            int randomIndex = rand.nextInt(n);
+            if (!catalogue.contains(recupJson[randomIndex])) {
+            	tmp.add(recupJson[randomIndex]);
+            }
+		}
+		
 		
 	}
 	
-	public void filtrerCatalogue(int option) {
+	
+	
+	public void accesResume(Utilisateur user, Film film) {
+		boolean statut = user.getStatutAbonnement();
+		String titre = film.getTitre();
+		String resume;
+		if (statut ==true) {
+			resume = film.getResume();
+			System.out.println("Résumé de "+titre+" : "+resume);
+		}
+		else {
+			System.out.println("Vous n'êtes pas abonné");
+		}
+		
+	
+	}
+	
+}
+
+		}
+		
 		
 	}
+	
 	
 	
 	public void accesResume(Utilisateur user, Film film) {
