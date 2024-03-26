aeffdaff611cd9b60852cb7380c1305a524097f6
VecteurOM <164197085+VecteurOM@users.noreply.github.com>
2024-03-26 22:36:07 +0100

    Acteur.java

--- Acteur.java
+++ Acteur.java
@@ -13,4 +13,17 @@ public class Acteur {
     this.nom = nom;
     this.prenom = prenom;
   }
+  
+  public String getNom() {
+    return nom;
+  }
+  public void setNom(String nom) {
+    this.nom=nom;
+  }
+  public String getPrenom() {
+     return prenom;
+  }
+  public void setPrenom(String prenom) {
+    this.prenom=prenom;
+  }
 }
