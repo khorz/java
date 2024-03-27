b98851eeb291e8122ccfc88f66ad8e4eb729e780
khorz <104204070+khorz@users.noreply.github.com>
2024-03-27 14:53:45 +0100

    Update Commentaire.java

--- Commentaire.java
+++ Commentaire.java
@@ -1,18 +1,18 @@
 package projet;
 
+import java.time.LocalDate;
 
 
-import java.util.Date;
 public class Commentaire {
 	private String auteur;
 	private String paragraphe;
-	private  Date date_com;
+	private  LocalDate date_com;
 	private int note;
 	
 	
 	
 	
-	public Commentaire(String auteur,Date date_adj,int note,String paragraphe) {
+	public Commentaire(String auteur,LocalDate date_adj,int note,String paragraphe) {
 		this.auteur=auteur;
 		this.date_com= date_adj;
 		this.note=note;
@@ -33,10 +33,10 @@ public class Commentaire {
 	public void setAuteur(String auteur) {
 		this.auteur = auteur;
 	}
-	public Date getDate_com() {
+	public LocalDate getDate_com() {
 		return date_com;
 	}
-	public void setDate_com(Date date_com) {
+	public void setDate_com(LocalDate date_com) {
 		this.date_com = date_com;
 	}
 	public int getNote() {
