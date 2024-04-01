bc3618269d8b642cf8da2fa9a74606a79b77d31e
xeu974 <164197253+xeu974@users.noreply.github.com>
2024-04-01 16:19:43 +0200

    Update Utilisateur.java

--- Utilisateur.java
+++ Utilisateur.java
@@ -1,130 +1,184 @@
-package Projet;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-import com.google.gson.JsonParseException;
-import java.util.HashMap;
+package projetFilmJava;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Comparator;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.*;
+//import java.sql.Date;
+//import java.util.ArrayList
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedWriter;
-import java.util.Date;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParseException;
+
 
 public class Utilisateur {
-    private String mail;
-    private String motDePasse;
-    private String nom;
-    private String prenom;
-    private String naissance;
-    private String adresse;
-    private String phraseSecrete;
-    private boolean statutAbonnement;
-    private int age;
+	private String mail;
+	private String motDePasse;
+	private String nom;
+	private String prenom;
+	private LocalDate naissance;
+	private String adresse;
+	private String phraseSecrete;
+	private boolean statutAbonnement;
+	private int age;
 
 
-    private Genre preferences;
+	private Genre preferences;
 
 
+	private Map<String,Commentaire> listeCommentairesPublies;
+	private List<Film> historique;
 
 
-    private Map<String,Commentaire> listeCommentairesPublies;
-    private List<Film> historique;
+	public Utilisateur(String mail, String motDePasse, String nom, String prenom, LocalDate naissance, String adresse, String phraseSecrete, int age) {
 
+		this.mail = mail;
+		this.motDePasse = motDePasse;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.naissance = naissance;
+		this.adresse = adresse;
+		this.phraseSecrete = phraseSecrete;
+		this.statutAbonnement = false;
+		this.age = age;
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Entrez numero de preference");
+		System.out.println("1)animation, 2)horreur, 3)drame, 4)comédie, 5)scienceFiction, 6)action, 7)documentaire, 8)thriller");
+		int index = scanner.nextInt();
+		while (index -1<0 || index-1>8) {
+			System.out.println("Entrez numero de preference");
+			index = scanner.nextInt();
 
-
-    public Utilisateur(String mail, String motDePasse, String nom, String prenom, String naissance, String adresse, String phraseSecrete, int age) {
-
-        this.mail = mail;
-        this.motDePasse = motDePasse;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.naissance = naissance;
-        this.adresse = adresse;
-        this.phraseSecrete = phraseSecrete;
-        this.statutAbonnement = false;
-        this.age = age;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Entrez numero de preference");
-        System.out.println("1)animation, 2)horreur, 3)drame, 4)comédie, 5)scienceFiction, 6)action, 7)documentaire, 8)thriller");
-        int index = scanner.nextInt();
-        while (index -1<0 || index-1>8) {
-            System.out.println("Entrez numero de preference");
-            index = scanner.nextInt();
-
-        }
+		}
         //scanner.close();
-        this.preferences = Genre.values()[index-1];
-        this.historique = new ArrayList<Film>();
-        this.listeCommentairesPublies = new HashMap<String,Commentaire>();
-        String chemin = "util.json";
-        EcrireJsonDirecte(this,chemin);
+		this.preferences = Genre.values()[index-1];
+		this.historique = new ArrayList<Film>();
+		this.listeCommentairesPublies = new HashMap<String,Commentaire>();
+		boolean ajt = ajouterUtilisateurJSON("Utilisateurs.json",this);
+		//boolean ajt = EcrireJsonDirecte(this,nomFichier);
 
-    }
+	}
 
-    public List<Film> getHistorique() {
-        return historique;
-    }
-
-
-    public void setHistorique(List<Film> historique) {
-        this.historique = historique;
-    }
-    public void ajouterFilmHistorique(Film film) {
-        this.historique.add(film);
-    }
+	static String nomFichier = "Utilisateurs.json";
+	public List<Film> getHistorique() {
+		return historique;
+	}
 
 
-    /*public static boolean EcrireJsonDirecte(Utilisateur user, String fichierJSON) {
-        try {
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String jsonStr = gson.toJson(user);
-            BufferedWriter bw = new BufferedWriter(new FileWriter(fichierJSON));
-            bw.write(jsonStr);
-            bw.close();
-        } catch (IOException e) {
-            System.out.println(e.getStackTrace() + " : Probleme de fichier");
-            return false;
-        } catch (JsonParseException e) {
-            System.out.println(e.getStackTrace() + " : JsonParseException");
-            return false;
-        }
-        return true;
-    }*/
-    public static boolean EcrireJsonDirecte(Utilisateur user, String fichierJSON) {
-        try {
-            Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-                    .setPrettyPrinting()
-                    .create();
-            String jsonStr = gson.toJson(user);
-            BufferedWriter bw = new BufferedWriter(new FileWriter(fichierJSON));
-            bw.write(jsonStr);
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Problème de fichier : " + e.getMessage());
-            return false;
-        }
-        return true;
-    }
+	public void setHistorique(List<Film> historique) {
+		this.historique = historique;
+	}
+
+	public void ajouterFilmHistorique(Film film) {
+		this.historique.add(film);
+	}
 
 
+	public static List<Utilisateur> lireTableauJSON(String nomFichier) {
+		List<Utilisateur> utilisateurs = new ArrayList<>();
+		JsonParser parser = new JsonParser();
+
+		try (FileReader reader = new FileReader(nomFichier)) {
+			// Utilisation de JsonParser pour parser le fichier JSON
+			JsonArray jsonArray = parser.parse(reader).getAsJsonArray();
+
+			// Parcourir les éléments du tableau JSON
+			for (JsonElement element : jsonArray) {
+				// Convertir chaque élément en objet Utilisateur en utilisant Gson
+				Utilisateur utilisateur = new Gson().fromJson(element, Utilisateur.class);
+				utilisateurs.add(utilisateur);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return utilisateurs;
+	}
+
+	public static List<Utilisateur> lireJSON(String fichierJSON) {
+		List<Utilisateur> utilisateurs = new ArrayList<>();
+		try {
+			Gson gson = new GsonBuilder()
+					.registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+					.create();
+			FileReader reader = new FileReader(fichierJSON);
+			utilisateurs = gson.fromJson(reader, new TypeToken<List<Utilisateur>>() {}.getType());
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichier non trouvé : " + e.getMessage());
+		} catch (JsonParseException e) {
+			System.out.println("Erreur de parsing JSON : " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Une erreur s'est produite : " + e.getMessage());
+		}
+		return utilisateurs;
+	}
+	public static boolean ajouterUtilisateurJSON(String nomFichier, Utilisateur nouvelUtilisateur) {
+		List<Utilisateur> utilisateurs = lireJSON(nomFichier);
+
+		// Ajouter le nouvel utilisateur à la liste
+		for (Utilisateur u : utilisateurs){
+			if(Objects.equals(nouvelUtilisateur.getMail(), u.getMail())){
+				System.out.println("Deja enregistre");
+				return false;
+			}
+		}
+		utilisateurs.add(nouvelUtilisateur);
+
+		// Convertir la liste mise à jour en JSON
+		Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
+		JsonArray jsonArray = new JsonArray();
+		for (Utilisateur utilisateur : utilisateurs) {
+			jsonArray.add(gson.toJsonTree(utilisateur));
+		}
+
+		// Écrire le contenu JSON dans le fichier
+		try (FileWriter file = new FileWriter(nomFichier)) {
+			gson.toJson(jsonArray, file);
+			file.flush();
+			System.out.println("Utilisateur ajouté avec succès.");
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean EcrireJsonDirecte(Utilisateur user, String fichierJSON) {
+		List<Utilisateur> utilisateurs = lireTableauJSON(nomFichier);
+
+		// Ajouter le nouvel utilisateur à la liste
+		for (Utilisateur u : utilisateurs){
+			if(Objects.equals(user.getMail(), u.getMail())){
+				System.out.println("Deja enregistre");
+				return false;
+			}
+		}
+		//utilisateurs.add(user);
+		try {
+			Gson gson = new GsonBuilder()
+					.registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+					.setPrettyPrinting()
+					.create();
+			String jsonStr = gson.toJson(user);
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fichierJSON,true));
+			bw.write(jsonStr);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Problème de fichier : " + e.getMessage());
+			return false;
+		}
+		return true;
+	}
 
 
-
-
-
-    // Getters
+	// Getters
     public String getMail() {
         return mail;
     }
@@ -141,7 +195,7 @@ public class Utilisateur {
         return prenom;
     }
 
-    public String getNaissance() {
+    public LocalDate getNaissance() {
         return naissance;
     }
 
@@ -165,11 +219,11 @@ public class Utilisateur {
         return preferences;
     }
 
-    public Map<String, Commentaire> getListeCommentairesPublies() {
-        return listeCommentairesPublies;
-    }
+	public Map<String, Commentaire> getListeCommentairesPublies() {
+		return listeCommentairesPublies;
+	}
 
-    // Setters
+	// Setters
     public void setMail(String mail) {
         this.mail = mail;
     }
@@ -186,7 +240,7 @@ public class Utilisateur {
         this.prenom = prenom;
     }
 
-    public void setNaissance(String naissance) {
+    public void setNaissance(LocalDate naissance) {
         this.naissance = naissance;
     }
 
@@ -210,37 +264,32 @@ public class Utilisateur {
         this.preferences = preferences;
     }
     public void setListeCommentairesPublies(Map<String, Commentaire> listeCommentairesPublies) {
-        this.listeCommentairesPublies = listeCommentairesPublies;
-    }
+		this.listeCommentairesPublies = listeCommentairesPublies;
+	}
 
-
+    
     //Méthode abonnement
 
-    public void sAbonner() {
-        if (this.statutAbonnement == true) {
-            System.out.println("Déjà abonné");
-        }
-        else {
-            this.statutAbonnement = true;
-            System.out.println("Abonné");
-        }
-    }
+	public void sAbonner() {
+		if (this.statutAbonnement) {
+			System.out.println("Déjà abonné");
+		} else {
+			this.statutAbonnement = true;
+			System.out.println("Abonné");
+		}
+	}
 
-    // Methode ajout de commentaires
-    public boolean ajouterCommentaire(Commentaire commentaire, String codeFilm) {
-        this.listeCommentairesPublies.put(codeFilm, commentaire);
-        return true;
-    }
+	// Methode ajout de commentaires
+	public boolean ajouterCommentaire(Commentaire commentaire, String codeFilm) {
+		this.listeCommentairesPublies.put(codeFilm, commentaire);
+		return true;
+	}
 
 
-    public static Genre valeurLaPlusFrequente(List<Genre> liste) {
+	public static Genre valeurLaPlusFrequente(List<Genre> liste) {
         // Créer une map pour stocker le nombre d'occurrences de chaque valeur
         Map<Genre, Integer> occurrences = new HashMap<>();
-        /*List<Genre> listeGenre = new ArrayList<Genre>();
-        for (Film film : liste) {
-        	Genre g = film.getGenre();
-        	listeGenre.add(g);
-        }*/
+
         // Parcourir la liste et compter les occurrences de chaque valeur
         for (Genre valeur : liste) {
             if (occurrences.containsKey(valeur)) {
@@ -264,55 +313,53 @@ public class Utilisateur {
     }
 
 
-    public Genre genrePrefere() {
-        List<Genre> tousLesGenres = new ArrayList<>();
-        for (Film element : this.historique) {
-            Genre genreDuFilm = element.getGenre();
-            tousLesGenres.add(genreDuFilm);
-        }
+	public Genre genrePrefere() {
+		List<Genre> tousLesGenres = new ArrayList<>();
+		for (Film element : this.historique) {
+			Genre genreDuFilm = element.getGenre();
+			tousLesGenres.add(genreDuFilm);
+		}
 
-        Genre genreLePlusRegarde = valeurLaPlusFrequente(tousLesGenres);
-        return genreLePlusRegarde;
-    }
+		Genre genreLePlusRegarde = valeurLaPlusFrequente(tousLesGenres);
+		return genreLePlusRegarde;
+	}
 
-    public void ajouterFilmHisto(Film film) {
-        historique.add(film);
-    }
+	public void ajouterFilmHisto(Film film) {
+		historique.add(film);
+	}
 
     public void trierParDate() {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Entrez numero de preference pour le tri");
-        System.out.println("1)Du plus vieux au plus récent, 2)Du plus récent au plus vieux");
-        int index = scanner.nextInt();
-        while (index !=2 && index!=1 ) {
-            System.out.println("Entrez numero de preference");
-            index = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Entrez numero de preference pour le tri");
+		System.out.println("1)Du plus vieux au plus récent, 2)Du plus récent au plus vieux");
+		int index = scanner.nextInt();
+		while (index !=2 && index!=1 ) {
+			System.out.println("Entrez numero de preference");
+			index = scanner.nextInt();
 
-        }
+		}
         //scanner.close();
         if (index == 1) {
-            this.historique.sort(Comparator.comparing(Film::getDateProd));
+			this.historique.sort(Comparator.comparing(Film::getDateProd));
 
         }
         else {
-            this.historique.sort(Comparator.comparing(Film::getDateProd).reversed());
+			this.historique.sort(Comparator.comparing(Film::getDateProd).reversed());
 
         }
 
-    }
+	}
 
-    public void visuInfos() {
-        System.out.println("Mail : "+this.mail);
-        System.out.println("Nom : " +this.nom);
-        System.out.println("Prenom : "+this.prenom);
-        System.out.println("Age : "+this.age);
+	public void visuInfos() {
+		System.out.println("Mail : "+this.mail);
+		System.out.println("Nom : " +this.nom);
+		System.out.println("Prenom : "+this.prenom);
+		System.out.println("Age : "+this.age);
 
-        System.out.println("Statut de l'abonnement : "+this.statutAbonnement);
+		System.out.println("Statut de l'abonnement : "+this.statutAbonnement);
 
-    }
-
-
+	}
 
 
 }
