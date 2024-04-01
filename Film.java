3901fbe7966f7f6204152e557cdf286b88260a6c
xeu974 <164197253+xeu974@users.noreply.github.com>
2024-04-01 22:58:57 +0200

    Update Film.java

--- Film.java
+++ Film.java
@@ -1,4 +1,4 @@
-package Projet;//import com.google.gson.Gson;
+package projetFilmJava;//import com.google.gson.Gson;
 
 import java.time.LocalDate;
 import java.io.*;
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.Comparator;
 
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 
 public class Film {
@@ -17,15 +18,15 @@ public class Film {
 	private String resume;
 	private float moyenne;
 	private Genre genre;
-	
+
 	private ArrayList<Commentaire> commentaires;
 	private Map<String, Commentaire> comByUser;
 	private boolean disable;
 	private List<Personne> acteurs;
 	private List<Personne> realisation;
-	
+
 	public Film(String code,String titre,LocalDate dateProd,String theme,float prix,String resume,float moyenne,Genre genre,List<Personne> acteurs,List<Personne> realisation) {
-		
+
 		this.code=code;
 		this.titre=titre;
 		this.dateProd=dateProd;
@@ -35,23 +36,24 @@ public class Film {
 		this.moyenne=moyenne;
 		this.genre=genre;
 		this.disable=false;
-		
+
 		this.commentaires= new ArrayList<Commentaire>();
 		this.comByUser = new HashMap<String, Commentaire>();
-		this.acteurs = acteurs;//ne vont pas changer 
+		this.acteurs = acteurs;//ne vont pas changer
 		this.realisation = realisation;
-		
-		
-		
-		 String chemin="listfilm.json";
-        // Créer un objet Gson
-		EcrireJsonDirecte(this,chemin);
+
+
+
+		String chemin="listfilm.json";
+		// Créer un objet Gson
+		ajouterFilmJSON(chemin,this);
+
 	}
 
-		
-		
-	
-	
+
+
+
+
 	public List<Personne> getActeurs() {
 		return acteurs;
 	}
@@ -99,7 +101,25 @@ public class Film {
 		return true;
 	}*/
 
-
+	public static List<Film> lireJSON(String fichierJSON) {
+		List<Film> films = new ArrayList<>();
+		try {
+			Gson gson = new GsonBuilder()
+					.registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+					.create();
+			FileReader reader = new FileReader(fichierJSON);
+			films = gson.fromJson(reader, new TypeToken<List<Film>>() {
+			}.getType());
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichier non trouvé : " + e.getMessage());
+		} catch (JsonParseException e) {
+			System.out.println("Erreur de parsing JSON : " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Une erreur s'est produite : " + e.getMessage());
+		}
+		return films;
+	}
 	public static boolean EcrireJsonDirecte(Film film, String fichierJSON) {
 		try {
 			Gson gson = new GsonBuilder()
@@ -113,7 +133,8 @@ public class Film {
 			// Crée un nouveau tableau JSON vide si le fichier n'existe pas encore
 			if (!file.exists()) {
 				jsonArray = new JsonArray();
-			} else {
+			}
+			else {
 				// Lecture du contenu existant du fichier JSON
 				JsonParser jsonParser = new JsonParser();
 				JsonElement jsonElement = jsonParser.parse(new FileReader(fichierJSON));
@@ -124,20 +145,28 @@ public class Film {
 
 					//vérifier si le film est déjà dedans
 
-					List<projetFilmJava.Film> films = new ArrayList<>();
+					List<projetFilmJava.Film> films = lireJSON("listfilm.json");
 					// Parcourir les éléments du tableau JSON
 					for (JsonElement element : jsonArray) {
 						// Convertir chaque élément en objet Utilisateur en utilisant Gson
 						projetFilmJava.Film film_liste = new Gson().fromJson(element, projetFilmJava.Film.class);
 						films.add(film_liste);
+
 					}
 					for(Film elt : films){
-						if( elt.getCode() == film.getCode()){
-							System.out.println("Le film exitse déjà dans la base de donnée ");
-							return false;
+						if(Objects.equals(elt.getCode(), film.getCode())){
+							films.remove(elt);
+							films.add(film);
+							System.out.println("Le film a ete mis a jour");
+							for (JsonElement element : jsonArray) {
+								// Convertir chaque élément en objet Utilisateur en utilisant Gson
+								projetFilmJava.Film film_liste = new Gson().fromJson(element, projetFilmJava.Film.class);
+								films.add(film_liste);
+							}
+							return true;
 						}
 					}
-					
+
 				} else {
 					// Si ce n'est pas un tableau JSON valide, crée un nouveau tableau JSON vide
 					jsonArray = new JsonArray();
@@ -165,42 +194,94 @@ public class Film {
 		}
 	}
 
+	public static boolean ajouterFilmJSON(String nomFichier, Film nouveauFilm) {
+		List<Film> films = lireJSON(nomFichier);
+
+		// Ajouter le nouvel film à la liste
+		for (Film f : films){
+			if(Objects.equals(nouveauFilm.getCode(), f.getCode())){
+				films.remove(f);
+				films.add(nouveauFilm);
+				//nouveauFilm.visuComFilm();
+				Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
+				JsonArray jsonArray = new JsonArray();
+				for (Film f2 : films) {
+					jsonArray.add(gson.toJsonTree(f2));
+				}
+
+				// Écrire le contenu JSON dans le fichier
+				try (FileWriter file = new FileWriter(nomFichier)) {
+					gson.toJson(jsonArray, file);
+					file.flush();
+					System.out.println("Film mis à jour");
+					return true;
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+
+			}
+		}
+		films.add(nouveauFilm);
+
+		// Convertir la liste mise à jour en JSON
+		Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
+		JsonArray jsonArray = new JsonArray();
+		for (Film f : films) {
+			jsonArray.add(gson.toJsonTree(f));
+		}
+
+		// Écrire le contenu JSON dans le fichier
+		try (FileWriter file = new FileWriter(nomFichier)) {
+			gson.toJson(jsonArray, file);
+			file.flush();
+			System.out.println("Film ajouté avec succès.");
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+
+
+
 	public boolean addCom( Utilisateur user) {// ou faire venir mail comme rm   boolean set
 		//si il l'a vu donc look historique
-		if(user.getHistorique().contains(this)) {		
+		if(user.getHistorique().contains(this)) {
 			//vérifie si les coms sont pas désactivés
-			if(!this.disable) {			
+			if(!this.disable) {
 				//vérifie si il a deja mis un com
 				if (!this.comByUser.containsKey(user.getMail()) ) {
-					
+
 					String auteur=user.getMail();
-					
+
 					Scanner scanner1 = new Scanner(System.in);
 					System.out.println("Entrez une note entre 0 et 10");
 
-				       
 					int note = scanner1.nextInt();
 					while (note < 0 || note > 10) {
 						System.out.println("Entrez une note entre 0 et 10");
 						note = scanner1.nextInt();
 					}
-					
+
 					scanner1 = new Scanner(System.in);
 					System.out.println("Ecrivez votre commentaire");
 					String paragraphe = scanner1.nextLine();
 					// ecrire note et paragraphe
-					
-					
-					//LocalDate aujourdhui = LocalDate.now();
-					Random rand = new Random(); int day = rand.nextInt(30 - 1 + 1) + 1;
+
+
+					LocalDate aujourdhui = LocalDate.now();
+					//Random rand = new Random(); int day = rand.nextInt(30 - 1 + 1) + 1;
 					//permet de tester filtrer par date
-					LocalDate aujourdhui = LocalDate.of(2023,5,day);
+					//LocalDate aujourdhui = LocalDate.of(2023,5,day);
 					Commentaire com = new Commentaire(auteur,aujourdhui,note,paragraphe);
-					
-					
+
+
 					this.commentaires.add(com);
 					this.comByUser.put(user.getMail(), com);
-					
+
+
 					//pour le mettre dans utilisateur
 					if (user.ajouterCommentaire(com,this.code)) {
 						System.out.println("Com ajouter");
@@ -211,10 +292,12 @@ public class Film {
 					}
 					//calculer moyenne
 					float moy = 0;
-					 for( Commentaire elt : this.commentaires) {
-						 moy=moy+ elt.getNote();
-					 }
-					 this.moyenne=moy/(this.commentaires.size());
+					for( Commentaire elt : this.commentaires) {
+						moy=moy+ elt.getNote();
+					}
+					this.moyenne=moy/(this.commentaires.size());
+					boolean maj = ajouterFilmJSON("listfilm.json",this);
+
 					return true;
 				}
 				System.out.println("Vous avez déjà mis un com");
@@ -226,43 +309,43 @@ public class Film {
 		System.out.println("vous n'avez pas encore vu le film");
 		return false;
 	}
-	
-	
+
+
 	public boolean rmCom(Utilisateur user) {//faire venir le mail du user
 		//vérifie si le user a mis un com
 		if (this.comByUser.containsKey(user.getMail()) ) {
-			
+
 			Commentaire com=this.comByUser.get(user.getMail());
 			this.commentaires.remove(com);
 			this.comByUser.remove(user.getMail());
 			//calculer moyenne
 			float moy = 0;
-			 for( Commentaire elt : this.commentaires) {
-				 moy=moy+ elt.getNote();
-			 }
-			 this.moyenne=moy/(this.commentaires.size());
-			 System.out.println("com supprimé");
+			for( Commentaire elt : this.commentaires) {
+				moy=moy+ elt.getNote();
+			}
+			this.moyenne=moy/(this.commentaires.size());
+			System.out.println("com supprimé");
 			return true;
-		}	
+		}
 		System.out.println("Vous n'avez pas de com sur ce film");
 		return false;
 	}
-	
-	
+
+
 	public boolean modifCom(Utilisateur user) {
 		if (this.comByUser.containsKey(user.getMail()) ) {
 			Commentaire com = this.comByUser.get(user.getMail());
-			
+
 			Scanner scanner1 = new Scanner(System.in);
 			System.out.println("Entrez une note entre 0 et 10");
 
-		       
+
 			int note = scanner1.nextInt();
 			while (note < 0 || note > 10) {
 				System.out.println("Entrez une note entre 0 et 10");
 				note = scanner1.nextInt();
 			}
-			
+
 			scanner1 = new Scanner(System.in);
 			System.out.println("Ecrivez votre commentaire");
 			String paragraphe = scanner1.nextLine();
@@ -274,29 +357,29 @@ public class Film {
 			System.out.println("Com modifié");
 			//calculer moyenne
 			float moy = 0;
-			 for( Commentaire elt : this.commentaires) {
-				 moy=moy+ elt.getNote();
-			 }
-			 this.moyenne=moy/(this.commentaires.size());
-			
+			for( Commentaire elt : this.commentaires) {
+				moy=moy+ elt.getNote();
+			}
+			this.moyenne=moy/(this.commentaires.size());
+
 			return true;
 		}
 		System.out.println("Vous n'avez pas de com sur ce film");
 		return false;
 	}
-	
+
 	public void visuComFilm() {
-		   for(Commentaire elem: this.commentaires)
-	       {
-	       	 System.out.println (elem.getAuteur());
-	       	 System.out.println (elem.getDate_com().toString());
-	       	 System.out.println (String.valueOf(elem.getNote()));
-	       	 System.out.println (elem.getParagraphe().concat("\n"));
-	       }
-		
+		for(Commentaire elem: this.commentaires)
+		{
+			System.out.println (elem.getAuteur());
+			System.out.println (elem.getDate_com().toString());
+			System.out.println (String.valueOf(elem.getNote()));
+			System.out.println (elem.getParagraphe().concat("\n"));
+		}
+
 	}
-	
-    public boolean isDisable() {
+
+	public boolean isDisable() {
 		return disable;
 	}
 
@@ -340,13 +423,13 @@ public class Film {
 		else {
 			this.commentaires.sort(Comparator.comparing(Commentaire::getDate_com).reversed());
 		}
-		
+
 		visuComFilm();
 	}
 
 
 
-    public void trierParNote() {
+	public void trierParNote() {
 		Scanner rev = new Scanner(System.in);
 		System.out.println("Pour l'avoir de la plus ancienne à la plus récente tapez 1 sinon 0");
 		int reverse = rev.nextInt();
@@ -360,38 +443,40 @@ public class Film {
 		else {
 			this.commentaires.sort(Comparator.comparing(Commentaire::getNote).reversed());
 		}
-		
+
 		visuComFilm();
-    }
-	public void visualiserPersonne(List<Personne> personnes) {
-		  for(Personne elem: personnes)
-	       {
-	       	 System.out.println (elem.getNom());
-	       	 System.out.println (elem.getPrenom().concat("\n"));
-	       }
 	}
-    
-    public void visuFilm(Utilisateur user) {
-    	System.out.println(this.titre);
-    	System.out.println(this.dateProd);
-    	System.out.println(this.theme);
-    	System.out.println(this.genre);
-    	System.out.println(this.prix);
-    	System.out.println(this.moyenne);
-    	
-    	if(user.getStatutAbonnement()) {
-    		System.out.println(this.resume);
-    	}
-    	visualiserPersonne(this.acteurs);
-    	visualiserPersonne(this.realisation);
-    	
-    	
-    }
-    
-    public void desactiverCom(boolean choix) {
-    	this.disable=choix;
-    }
-    
+	public void visualiserPersonne(List<Personne> personnes) {
+		for(Personne elem: personnes)
+		{
+			System.out.println (elem.getNom());
+			System.out.println (elem.getPrenom().concat("\n"));
+		}
+	}
+
+	public void visuFilm(Utilisateur user) {
+		System.out.println( "Titre : " + titre + "\n" +
+				"Date de production : " + dateProd + "\n" +
+				"Thème : " + theme + "\n" +
+				"Genre : " + genre + "\n" +
+				"Prix : " + prix + "\n" +
+				"Moyenne : " + moyenne + "\n" +
+				"Acteurs : " + acteurs + "\n" +
+				"Réalisateurs : " + realisation + "\n");
+
+		if(user.getStatutAbonnement()) {
+			System.out.println("Resume : "+ this.resume);
+		}
+		visualiserPersonne(this.acteurs);
+		visualiserPersonne(this.realisation);
+
+
+	}
+
+	public void desactiverCom(boolean choix) {
+		this.disable=choix;
+	}
+
 	public String getCode() {
 		return code;
 	}
@@ -434,9 +519,9 @@ public class Film {
 	public void setMoyenne(float moyenne) {
 		this.moyenne = moyenne;
 	}
-	
-	
-	
+
+
+
 	public Genre getGenre() {
 		return genre;
 	}
