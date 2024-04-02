35d846507e88fa3da3a922a9c53fa41837e83575
xeu974 <164197253+xeu974@users.noreply.github.com>
2024-04-02 11:17:53 +0200

    Create PersonneTest.java

---
+++ PersonneTest.java
@@ -0,0 +1,24 @@
+package projet.src;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonneTest {
+
+    @Test
+    public void getNom() {
+    }
+
+    @Test
+    public void setNom() {
+    }
+
+    @Test
+    public void getPrenom() {
+    }
+
+    @Test
+    public void setPrenom() {
+    }
+}
