0f5575407f5a572d4ee9957220263fd082b19c20
xeu974 <164197253+xeu974@users.noreply.github.com>
2024-04-02 11:16:12 +0200

    Create CommentaireTest.java

---
+++ CommentaireTest.java
@@ -0,0 +1,40 @@
+package projet.src;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommentaireTest {
+
+    @Test
+    public void getParagraphe() {
+    }
+
+    @Test
+    public void setParagraphe() {
+    }
+
+    @Test
+    public void getAuteur() {
+    }
+
+    @Test
+    public void setAuteur() {
+    }
+
+    @Test
+    public void getDate_com() {
+    }
+
+    @Test
+    public void setDate_com() {
+    }
+
+    @Test
+    public void getNote() {
+    }
+
+    @Test
+    public void setNote() {
+    }
+}
